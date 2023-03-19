e5280719a95fdfea5b6d5eab18757286ce903a26
Viktor Tagil <viktor.tagil@gmail.com>
2023-03-18 17:13:27 -0700

    authcontext

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/authContext';
 
 type Props = {}
 
 const Navigation = (props: Props) => {
+
+  const { user, setUser } = useContext(AuthContext);
+  console.log("user", user)
+
+
+  const login = () => {
+    setUser(
+      {
+        name: "viktor",
+        email: "viktoremail"
+      }
+    )
+  }
+
+   const logout = () => {
+    setUser({})
+  }
+
   return (
-    <div>Navigation</div>
+    <>
+      <nav>
+
+      </nav>
+
+      <button onClick={login}>login</button>
+      <button onClick={logout}>login</button>
+      </>
   )
 }
 
